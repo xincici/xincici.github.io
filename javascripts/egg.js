d9a612d7db780ebe7347283272acb412f285e818
linye <yelin@sohu-inc.com>
2014-07-15 16:19:05 +0800

    update egg.js

--- javascripts/egg.js
+++ javascripts/egg.js
@@ -1,20 +1,22 @@
 $(function(){
 	var keystr = '383840403737393966656665';
-	var everFalse = false;
 	var pressed = '';
+    var shown = false;
 	$(document).on('keyup', function(e){
-		if(everFalse) return;
+        if( shown ) return;
 		pressed = pressed + e.keyCode;
+        pressed = pressed.slice(-24);
 		if( pressed == keystr ){
+            shown = true;
 			$.ydialog({
 				title : 'download resume',
 				simple : true,
 				type : 'alert',
-				content : '<a target="blank" href="/staff/resume.pdf">click here to download resume</a>'
+				content : '<a target="blank" href="/staff/resume.pdf">click here to download resume</a>',
+                close : function(){
+                    shown = false;
+                }
 			});
-			everFalse = true;
-		}else if(pressed.length > keystr.length){
-			everFalse = true;
 		}
 	});
 });
