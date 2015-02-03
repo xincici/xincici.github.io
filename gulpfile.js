9cf5fac4e5af1ae946822b202768e97806d2dd3e
linye <linye@meituan.com>
2015-02-03 18:35:38 +0800

    use gulp file include

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+var gulp = require('gulp');
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+var watchify = require('watchify');
+
+var fileinclude = require('gulp-file-include');
+gulp.task('html', function() {
+    gulp.src('app/*.html')
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+    }))
+    .pipe(gulp.dest(''));
+});
+// Default task
+gulp.task('default', ['html']);
