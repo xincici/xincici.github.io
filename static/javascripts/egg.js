6a854e5d54b1e64bca3c41fc92a72600bb6c4a43
linye <linye@meituan.com>
2016-02-25 10:33:15 +0800

    xxx

--- static/javascripts/egg.js
+++ static/javascripts/egg.js
@@ -1,23 +1,23 @@
 $(function(){
     // 上上下下左左右右BABA
-	var keystr = '383840403737393966656665';
-	var pressed = '';
+    var keystr = '383840403737393966656665';
+    var pressed = '';
     var shown = false;
-	$(document).on('keyup', function(e){
+    $(document).on('keyup', function(e){
         if( shown ) return;
-		pressed = pressed + e.keyCode;
+        pressed = pressed + e.keyCode;
         pressed = pressed.slice(-24);
-		if( pressed == keystr ){
+        if( pressed == keystr ){
             shown = true;
-			$.ydialog({
-				title : 'download resume',
-				simple : true,
-				type : 'alert',
-				content : '<a target="blank" href="/resume/resume.pdf">click here to download resume</a>',
+            $.ydialog({
+                title : 'download resume',
+                simple : true,
+                type : 'alert',
+                content : '<a target="blank" href="/resume/resume.pdf">click here to download resume</a>',
                 close : function(){
                     shown = false;
                 }
-			});
-		}
-	});
+            });
+        }
+    });
 });
